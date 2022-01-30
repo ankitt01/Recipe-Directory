3536163b0df472c6597e2f2d1bdbd11e1d362269
ankitt01 <ankitrawat0102@gmail.com>
2022-01-30 19:24:42 +0530

    fetching data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
 import './Home.css'
+import {useFetch} from '../../hooks/useFetch'
 
 export default function Home() {
+
+    const {data, isPending, error} = useFetch('http://127.0.0.1:5500/data/db.json')
+    console.log(data) 
     return (
-        <div>
-            Home Page
+        <div className='home'>
+            {/* {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading..</p>} */}
+            {data && data.recipes.map(recipe => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
         </div>
     )
 }
